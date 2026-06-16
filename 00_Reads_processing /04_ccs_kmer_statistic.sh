7559ab6e5af3a204ce6c1782f5c43cd33ca41bdb
Francine Almeida <139783093+almeidafran@users.noreply.github.com>
2026-06-16 13:14:47 -0300

    Create 04_ccs_kmer_statistic.sh

---
+++ 00_Reads_processing /04_ccs_kmer_statistic.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#############################################
+# K-mer analysis with Jellyfish and GenomeScope
+#############################################
+
+# Determine the optimal k-mer size for Merqury
+# Script available at:
+# https://github.com/marbl/merqury/blob/master/best_k.sh
+
+./best_k.sh <genome_size>
+
+#############################################
+# Count k-mers from PacBio HiFi reads
+#############################################
+bestk=19 #Replace with the optimal k-mer size estimated by best_k.sh, or to your preference.
+jellyfish count \
+    -C \
+    -m $bestk \
+    -s 1000000000 \
+    -t 24 \
+    hifi_reads.fq \
+    -o hifi_reads.jf
+
+#############################################
+# Generate k-mer frequency histogram
+#############################################
+
+jellyfish histo \
+    -t 24 \
+    hifi_reads.jf \
+    > hifi_reads.histo
+
+#############################################
+# GenomeScope analysis
+#############################################
+
+# Upload the histogram file (hifi_reads.histo)
+# to GenomeScope:
+#
+# http://genomescope.org
+#
+# GenomeScope estimates:
+#   - Genome size
+#   - Heterozygosity
+#   - Repeat content
+#   - Sequencing error rate
+#
+# based on the k-mer frequency distribution.
