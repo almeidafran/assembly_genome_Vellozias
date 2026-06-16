f4d9e27bfe31dc2ebe9b86f93dabb3f58a65be71
Francine Almeida <139783093+almeidafran@users.noreply.github.com>
2026-06-16 12:45:15 -0300

    Create 02_ccs_filter.sh

---
+++ 00_Reads_processing /02_ccs_filter.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+hifireads=/file_path/ccs.fastq.gz
+
+##### activate conda environment #####
+#conda activate yahs
+
+##### adapter filter #####
+hifiadapterfilt.sh -p hifireads -t 16 -o filter_adapter/
