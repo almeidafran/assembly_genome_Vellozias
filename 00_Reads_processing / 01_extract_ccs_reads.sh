5492e40d8dc986860ce123431331939c3e29c3aa
Francine Almeida <139783093+almeidafran@users.noreply.github.com>
2026-06-16 12:40:46 -0300

    Update  01_extract_ccs_reads.sh

--- 00_Reads_processing / 01_extract_ccs_reads.sh
+++ 00_Reads_processing / 01_extract_ccs_reads.sh
@@ -1,1 +1,17 @@
+# !/bin/bash
+
+# Generate PacBio CCS (HiFi) reads from raw subreads
+# Input: PacBio subreads BAM file
+# Output: Compressed FASTQ file containing high-fidelity (HiFi) consensus reads
+#
+# The CCS algorithm combines multiple passes of the same DNA molecule
+# to generate highly accurate consensus sequences. In this example,
+# only reads with at least three passes are retained.
+
+##### activate conda environment #####
+conda activate yahs
+
+##### extract ccs reads #####
+nohup ccs raw.subreads.bam ccs.fastq.gz --min-passes 3 \
+--report-file ccs_report.txt --log-file ccs.log -j 32 &
 
