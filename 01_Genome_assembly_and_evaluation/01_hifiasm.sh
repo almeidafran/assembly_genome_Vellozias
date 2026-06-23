3ab0ea85c1180427b80ddabfa861a0adb0e9b000
Francine Almeida <139783093+almeidafran@users.noreply.github.com>
2026-06-23 17:56:46 -0300

    Update 01_hifiasm.sh

--- 01_Genome_assembly_and_evaluation/01_hifiasm.sh
+++ 01_Genome_assembly_and_evaluation/01_hifiasm.sh
@@ -1,1 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
 
+#############################################
+# Run hifiasm with PacBio HiFi + Hi-C reads
+#############################################
+
+# Input files
+HIFI="hifi.fq.gz"
+HIC_R1="hic.read1.fq.gz"
+HIC_R2="hic.read2.fq.gz"
+
+# Output prefix
+PREFIX="output.asm"
+
+# Number of threads
+THREADS=24
+
+# Run hifiasm
+hifiasm \
+    -o "$PREFIX" \
+    -t "$THREADS" \
+    --h1 "$HIC_R1" \
+    --h2 "$HIC_R2" \
+    "$HIFI"
+
+#############################################
+# Convert GFA output files to FASTA
+#############################################
+
+# Primary contigs
+awk '/^S/{print ">"$2; print $3}' "${PREFIX}.bp.p_ctg.gfa" > "${PREFIX}.bp.p_ctg.fa"
+
+# Haplotype-resolved contigs
+awk '/^S/{print ">"$2; print $3}' "${PREFIX}.bp.hap1.p_ctg.gfa" > "${PREFIX}.bp.hap1.p_ctg.fa"
+awk '/^S/{print ">"$2; print $3}' "${PREFIX}.bp.hap2.p_ctg.gfa" > "${PREFIX}.bp.hap2.p_ctg.fa"
