c05e02e946dc24e5b4e50cc67cb012b49eef2db8
Francine Almeida <139783093+almeidafran@users.noreply.github.com>
2026-06-24 18:47:33 -0300

    Update 03_ccs_reads_statistic.sh

--- 00_Reads_processing /03_ccs_reads_statistic.sh
+++ 00_Reads_processing /03_ccs_reads_statistic.sh
@@ -6,3 +6,11 @@ conda activate yahs
 
 ##### simple statistics of FASTA/Q files #####
 seqkit stats -a -j 2 $filt_hifireads > ccs.filt.fastq.stats.txt
+
+
+####Check the quality and size of the reads; if necessary, apply a size and quality filter using filtlong.####
+#https://github.com/rrwick/filtlong
+filtlong \
+    --min_mean_q 20 \
+    --min_length 10000 \
+    $filt_hifireads > hifi.filt.q20.10kb.fastq
