c1c92c7050ccd81f03cb8f54bfffdd9679f7b08d
Francine Almeida <139783093+almeidafran@users.noreply.github.com>
2026-06-24 12:46:42 -0300

    Update 02_ccs_filter.sh

--- 00_Reads_processing /02_ccs_filter.sh
+++ 00_Reads_processing /02_ccs_filter.sh
@@ -4,5 +4,6 @@ hifireads=/file_path/ccs.fastq.gz
 ##### activate conda environment #####
 #conda activate yahs
 
+#Get script https://github.com/sheinasim-USDA/HiFiAdapterFilt/blob/master/hifiadapterfilt.sh
 ##### adapter filter #####
 hifiadapterfilt.sh -p $hifireads -t 16 -o filter_adapter/
